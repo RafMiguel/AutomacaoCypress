e747506dc2d1277045f202b39355367109fbbe8b
RafMiguel <rafaelmiguel11@hotmail.com>
2023-02-05 17:13:27 -0300

    Reestruturamento e limpeza no código / adicionado o plugin cypress-image-snapshot (ainda não implementado na spec de testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,29 @@
 const { defineConfig } = require("cypress");
+const {
+    addMatchImageSnapshotPlugin,
+} = require("cypress-image-snapshot/plugin");
 
 module.exports = defineConfig({
-  e2e: {
-    viewportHeight:1366,
-    viewportWidth:768,
-    baseUrl:'https://phptravels.com/demo',
-    chromeWebSecurity:false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        viewportWidth: 768,
+        viewportHeight: 1366,
+        baseUrl: "https://phptravels.com/",
+        chromeWebSecurity: false,
+        videosFolder: "cypress/validation/videos",
+        screenshotsFolder: "cypress/validation/screenshots",
+        reporter: "mochawesome",
+        reporterOptions: {
+            reportDir: "cypress/validation/report",
+            reportFilename: "phptravels",
+            overwrite: false,
+            html: true,
+            json: true,
+            timestamp: "dd-mm-yyyy_HH-MM",
+        },
     },
-  },
+
+    setupNodeEvents(on, config) {
+        addMatchImageSnapshotPlugin(on, config);
+        // implement node event listeners here
+    },
 });
--- cypress/e2e/spec_demo.cy.js
+++ cypress/e2e/spec_demo.cy.js
@@ -1,43 +1,53 @@
 /// <reference types="cypress"/>
 
+import { elDemo } from "../support/elementos";
 
-describe('Validações PHPTravels', () => {
+import dados_cliente from "../fixtures/dados_cliente.json";
 
+describe("Validações PHPTravels", () => {
+    context("Página Demo", () => {
+        before(() => {
+            cy.visit("/demo"); //.screenshot("Demo - Home");
+        });
+        it("Preencher formulário", () => {
+            cy.xpath(elDemo.campo_first_name)
+                .should("have.attr", "placeholder", "First Name")
+                .type(dados_cliente.first_name, { force: true });
 
-  it('sdsdsd', () => {
-    cy.visit('/')
-    cy.formulario()
-/*
-cy.get('.df').find('#numb1').invoke('text').then((numb1) => {
-  expect(numb1).match(/^[0-9]*$/); // Espero que a variável 'numb1' (que está armazenando o valor (texto) contido em #numb1) combine com o resultado que esteja entre 0 e 9 (/^[0-9]*$/)
+            cy.xpath(elDemo.campo_last_name)
+                .should("have.attr", "placeholder", "Last Name")
+                .type(dados_cliente.last_name, { force: true });
+            cy.xpath(elDemo.campo_business)
+                .should("have.attr", "placeholder", "Business Name")
+                .type(dados_cliente.business, { force: true });
+            cy.xpath(elDemo.campo_email)
+                .should("have.attr", "placeholder", "Email")
+                .type(dados_cliente.email, { force: true });
 
-cy.get('.df').find('#numb2').invoke('text').then((numb2) => {
-expect(numb2).match(/^[0-9]*$/)
+            //Armazenar o valor dos números dentro dos elementos #numb1 e #numb2 para utiliza-los numa soma e gerar o resultado requerido
+            cy.get(elDemo.digitos_resultado)
+                .find(elDemo.primeiro_digito)
+                .invoke("text")
+                .then((numb1) => {
+                    expect(numb1).match(/^[0-9]*$/); // Espero que a variável 'numb1' (que está armazenando o valor (texto) contido em #numb1) combine com o resultado que esteja entre 0 e 9 (/^[0-9]*$/)
+                    cy.get(elDemo.digitos_resultado)
+                        .find(elDemo.segundo_digito)
+                        .invoke("text")
+                        .then((numb2) => {
+                            expect(numb2).match(/^[0-9]*$/);
 
-//Abaixo foi criada duas variaveis (n1_int e n2_int) que servirão para convertermos (através do parseInt) os textos obtidos das variaveis numb1 e numb2 em números inteiros para podermos somá-los.
-//Sem converter os textos obtidos em números inteiros, o resultado da soma seria simplesmente a junção do valor (texto) de numb1 com o valor (texto) de numb2. Exemplo: 5(numb1) + 5(numb2), sem convertermos as variaveis para int, traria o resultado 55 ao invés de 10.
-var n1_int = parseInt(numb1)
-var n2_int = parseInt(numb2)
-var result = n1_int + n2_int
+                            //Abaixo foi criada duas variaveis (n1_int e n2_int) que servirão para convertermos (através do parseInt) os textos obtidos das variaveis numb1 e numb2 em números inteiros para podermos somá-los.
+                            //Sem converter os textos obtidos em números inteiros, o resultado da soma seria simplesmente a junção do valor (texto) de numb1 com o valor (texto) de numb2. Exemplo: 5(numb1) + 5(numb2), sem convertermos as variaveis para int, traria o resultado 55 ao invés de 10.
+                            var n1_int = parseInt(numb1);
+                            var n2_int = parseInt(numb2);
+                            var result = n1_int + n2_int;
 
-cy.get('#number').type(result,{force:true})
-  })
-  
-})*/
-
-  });
-
-
-
-
-
-
-
-
-/*
-let numb1 = cy.xpath('.//span[contains(@id, "numb1")]')
-cy.xpath('.//input[@id="number"]').type(numb1)
-console.log($numb1.te)
-*/
-
-})
+                            cy.get(elDemo.campo_resultado)
+                                .should("have.attr", "placeholder", "Result ?")
+                                .type(result, { force: true });
+                        });
+                });
+            //cy.screenshot("Demo - Form");
+        });
+    });
+});
